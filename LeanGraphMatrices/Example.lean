5fe269651d26b54a0a6cc5514df521fb463363b1
Harry Richman <dhrichman@gmail.com>
2025-04-27 10:47:34 -0400

    add lean file with explicit graph and matrix calculations

--- LeanGraphMatrices/Example.lean
+++ LeanGraphMatrices/Example.lean
@@ -1,4 +1,4 @@
-import Mathlib.Combinatorics.SimpleGraph.Basic
+import Mathlib.Combinatorics.SimpleGraph.Finite
 import Mathlib.Combinatorics.SimpleGraph.LapMatrix
 import Mathlib.LinearAlgebra.Matrix.Determinant.Basic
 
@@ -12,41 +12,46 @@ example : Bool := True
 example : Bool := True
 
 -- edge set of house graph
-inductive HGE : (Fin 5) → (Fin 5) → Prop
-  | e01 : HGE 0 1
-  | e12 : HGE 1 2
-  | e23 : HGE 2 3
-  | e34 : HGE 3 4
-  | e40 : HGE 4 0
-  | e14 : HGE 1 4
-  -- reverse edges
-  | e04 : HGE 0 4
-  | e10 : HGE 1 0
-  | e21 : HGE 2 1
-  | e32 : HGE 3 2
-  | e43 : HGE 4 3
-  | e41 : HGE 4 1
+def hge : (Fin 5) → (Fin 5) → Bool
+  | 0, 1 => true
+  | 1, 2 => true
+  | 2, 3 => true
+  | 3, 4 => true
+  | 4, 0 => true
+  | 1, 4 => true
+  | _, _ => false
 
-#check HGE
+#check hge
+#eval hge 0 1
+#eval hge 0 2
 
-def HouseGraph : SimpleGraph (Fin 5) where
-  Adj := HGE
-  symm := by {-- unfold Symmetric
-    intros v w edge
-    rcases edge
-    repeat constructor
+def houseGraph : SimpleGraph (Fin 5) where
+  Adj v w := hge v w || hge w v
+  symm := by {
+    dsimp [Symmetric]
+    decide
   }
-  loopless := by {-- unfold Irreflexive
-    intros v loop
-    rcases loop
+  loopless := by {
+    dsimp [Irreflexive]
+    decide
   }
 
-#check HouseGraph.edgeSet
+-- seems to be required to `#eval` number of edges
+instance : DecidableRel houseGraph.Adj :=
+  fun a b => inferInstanceAs <| Decidable (hge a b || hge b a)
 
--- seems to be required by lapMatrix
-variable [DecidableRel HouseGraph.Adj]
+#check houseGraph.edgeSet
+#check houseGraph.edgeFinset
+
+def num_edges := houseGraph.edgeFinset.card
+#eval num_edges -- 6
 
 /-- Laplacian matrix of a graph -/
+
+def L := houseGraph.lapMatrix ℤ
+
+#check L
+#eval L
 
 def L' : Matrix (Fin 5) (Fin 5) ℤ :=
   !![2, -1,  0,  0, -1;
@@ -54,16 +59,11 @@ def L' : Matrix (Fin 5) (Fin 5) ℤ :=
      0, -1,  2, -1,  0;
      0,  0, -1,  2, -1;
     -1, -1,  0, -1,  3]
+#eval L == L'
 
-def L := HouseGraph.lapMatrix ℤ
+#check L.det
+#eval L.det -- 0
 
-#check L
-#eval L
-
-#check Matrix.det L
-
--- #eval Matrix.det L'
-#eval L'.det
 
 /-- Reduced Laplacian matrix -/
 
@@ -72,10 +72,13 @@ def L := HouseGraph.lapMatrix ℤ
 -- We can use Fin.castLEEmb for this (requires proof 4 <= 5)
 def inc₄₅ : Fin 4 ↪ Fin 5 := Fin.castLEEmb (by decide : 4 ≤ 5)
 
-def L0' : Matrix (Fin 4) (Fin 4) ℤ :=
-  L'.submatrix inc₄₅ inc₄₅
+def L0 : Matrix (Fin 4) (Fin 4) ℤ :=
+  L.submatrix inc₄₅ inc₄₅
 
-#check L0'
-#eval L0'
+#check L0
+#eval L0
 
-#eval L0'.det
+#eval L0.det -- 11
+
+
+/-- Set of spanning trees of G -/
